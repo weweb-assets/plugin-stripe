5128338643b5f3d368dec0244d243de6c607b512
Dams-d <damien.duro8@gmail.com>
2022-06-24 10:41:36 +0200

    Add stripe instance

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -2,5 +2,25 @@
 import './components/Configuration/SettingsEdit.vue';
 import './components/Configuration/SettingsSummary.vue';
 /* wwEditor:end */
+import Stripe from 'stripe';
 
-export default {};
+export default {
+    instance: null,
+    /*=============================================m_ÔÔ_m=============================================\
+        Plugin API
+    \================================================================================================*/
+    async onLoad(settings) {
+        /* wwFront:start */
+        await this.load(settings.publicData.publicApiKey);
+        /* wwFront:end */
+        /* wwEditor:start */
+        await this.load(settings.publicData.publicTestApiKey || settings.publicData.publicApiKey);
+        /* wwEditor:end */
+    },
+    /*=============================================m_ÔÔ_m=============================================\
+        Stripe API
+    \================================================================================================*/
+    async load(publicApiKey) {
+        this.instance = Stripe(publicApiKey);
+    },
+};
