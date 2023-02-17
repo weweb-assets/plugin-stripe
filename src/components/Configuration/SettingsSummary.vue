e9345d8f49e4a8b655a1248e007123ffc5eb69eb
Dams-d <damien.duro8@gmail.com>
2023-02-17 11:26:01 +0100

    Fix

--- src/components/Configuration/SettingsSummary.vue
+++ src/components/Configuration/SettingsSummary.vue
@@ -2,22 +2,22 @@
     <wwEditorFormRow label="Public API key">
         <div class="flex items-center body-2">
             <wwEditorIcon name="key" class="mr-2" />
-            <div class="text-ellipsis">{{ settings.publicData.publicApiKey }}</div>
+            <div class="text-ellipsis">{{ settings.publicData.publicApiKey || 'not defined' }}</div>
         </div>
     </wwEditorFormRow>
     <wwEditorFormRow label="Private API key">
         <div class="flex items-center body-2">
             <wwEditorIcon name="key" class="mr-2" />
-            <div class="text-ellipsis">{{ privateApiKey }}</div>
+            <div class="text-ellipsis">{{ privateApiKey || 'not defined' }}</div>
         </div>
     </wwEditorFormRow>
-    <wwEditorFormRow label="Public test API key">
+    <wwEditorFormRow label="Public test API key" v-if="settings.publicData.publicTestApiKey">
         <div class="flex items-center body-2">
             <wwEditorIcon name="key" class="mr-2" />
             <div class="text-ellipsis">{{ settings.publicData.publicTestApiKey }}</div>
         </div>
     </wwEditorFormRow>
-    <wwEditorFormRow label="Private test API key">
+    <wwEditorFormRow label="Private test API key" v-if="privateTestApiKey">
         <div class="flex items-center body-2">
             <wwEditorIcon name="key" class="mr-2" />
             <div class="text-ellipsis">{{ privateTestApiKey }}</div>
@@ -32,10 +32,10 @@ export default {
     },
     computed: {
         privateApiKey() {
-            return this.settings.privateData.privateApiKey.replace(/./g, '*');
+            return (this.settings.privateData.privateApiKey || '').replace(/./g, '*');
         },
         privateTestApiKey() {
-            return this.settings.privateData.privateTestApiKey.replace(/./g, '*');
+            return (this.settings.privateData.privateTestApiKey || '').replace(/./g, '*');
         },
     },
 };
