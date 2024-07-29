0de640ffbbe482f1cba57b7b7f5c2495335b62cb
joelherzog <121586608+joelherzog@users.noreply.github.com>
2024-07-29 10:27:16 +0200

    add Twint payment method to checkout
    https://docs.stripe.com/payments/twint/accept-a-payment?platform=web&ui=stripe-hosted

--- src/components/Functions/Checkout.vue
+++ src/components/Functions/Checkout.vue
@@ -253,6 +253,7 @@ export default {
                 { label: 'SEPA debit', value: 'sepa_debit' },
                 { label: 'Sofort', value: 'sofort' },
                 { label: 'WeChat Pay', value: 'wechat_pay' },
+                { label: 'Twint', value: 'twint' },
             ],
             enableOptions: [
                 { label: 'Enable', value: true },
