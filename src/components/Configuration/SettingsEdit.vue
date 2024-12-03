4f211746568d9a93297963c3d3fddce3a4212675
Alexy Carlier <58709996+alexyca@users.noreply.github.com>
2024-12-03 15:07:26 +0100

    Ww 3132 stripe plugin using incorrect api keys in staging environment (#15)
    * Add staging api key to config and setting
    
    * Update warning logic for missed keys

--- src/components/Configuration/SettingsEdit.vue
+++ src/components/Configuration/SettingsEdit.vue
@@ -1,4 +1,10 @@
 <template>
+    <div v-if="!isEveryKeysSet" class="mb-3 label-sm content-warning flex items-center">
+        <wwEditorIcon class="mr-1" name="warning" small />
+        STAGING and EDITOR use PRODUCTION keys if empty
+    </div>
+    <!-- PROD -->
+    <div class="my-3 label-sm flex items-center">Production</div>
     <wwEditorFormRow required label="Public API key">
         <wwEditorInputText
             placeholder="pk_live_********"
@@ -14,14 +20,33 @@
             @update:modelValue="setPrivateApiKey"
         />
     </wwEditorFormRow>
-    <wwEditorFormRow label="Public test API key">
+    <!-- STAGING -->
+    <div class="my-3 label-sm flex items-center">Staging</div>
+    <wwEditorFormRow label="Public API key">
+        <wwEditorInputText
+            placeholder="pk_test_********"
+            :model-value="settings.publicData.publicStagingApiKey"
+            @update:modelValue="setPublicStagingApiKey"
+        />
+    </wwEditorFormRow>
+    <wwEditorFormRow label="Private API key">
+        <wwEditorInputText
+            type="password"
+            placeholder="sk_test_********"
+            :model-value="settings.privateData.privateStagingApiKey"
+            @update:modelValue="setPrivateStagingApiKey"
+        />
+    </wwEditorFormRow>
+    <!-- EDITOR -->
+    <div class="my-3 label-sm flex items-center">Editor</div>
+    <wwEditorFormRow label="Public API key">
         <wwEditorInputText
             placeholder="pk_test_********"
             :model-value="settings.publicData.publicTestApiKey"
             @update:modelValue="setPublicTestApiKey"
         />
     </wwEditorFormRow>
-    <wwEditorFormRow label="Private test API key">
+    <wwEditorFormRow label="Private API key">
         <wwEditorInputText
             type="password"
             placeholder="sk_test_********"
@@ -38,14 +63,20 @@ export default {
     },
     emits: ['update:settings'],
     methods: {
+        reloadPlugin() {
+            this.plugin.load(
+                this.settings.publicData.publicTestApiKey ||
+                    this.settings.publicData.publicStagingApiKey ||
+                    this.settings.publicData.publicApiKey
+            );
+        },
+        // used in prod
         setPublicApiKey(publicApiKey) {
             this.$emit('update:settings', {
                 ...this.settings,
                 publicData: { ...this.settings.publicData, publicApiKey },
             });
-            this.$nextTick(() =>
-                this.plugin.load(this.settings.publicData.publicTestApiKey || this.settings.publicData.publicApiKey)
-            );
+            this.$nextTick(() => this.reloadPlugin());
         },
         setPrivateApiKey(privateApiKey) {
             this.$emit('update:settings', {
@@ -53,20 +84,45 @@ export default {
                 privateData: { ...this.settings.privateData, privateApiKey },
             });
         },
+        // used in staging
+        setPublicStagingApiKey(publicStagingApiKey) {
+            this.$emit('update:settings', {
+                ...this.settings,
+                publicData: { ...this.settings.publicData, publicStagingApiKey },
+            });
+            this.$nextTick(() => this.reloadPlugin());
+        },
+        setPrivateStagingApiKey(privateStagingApiKey) {
+            this.$emit('update:settings', {
+                ...this.settings,
+                privateData: { ...this.settings.privateData, privateStagingApiKey },
+            });
+        },
+        // used in editor
         setPublicTestApiKey(publicTestApiKey) {
             this.$emit('update:settings', {
                 ...this.settings,
                 publicData: { ...this.settings.publicData, publicTestApiKey },
             });
-            this.$nextTick(() =>
-                this.plugin.load(this.settings.publicData.publicTestApiKey || this.settings.publicData.publicApiKey)
-            );
+            this.$nextTick(() => this.reloadPlugin());
         },
         setPrivateTestApiKey(privateTestApiKey) {
             this.$emit('update:settings', {
                 ...this.settings,
                 privateData: { ...this.settings.privateData, privateTestApiKey },
             });
+        },
+    },
+    computed: {
+        isEveryKeysSet() {
+            return (
+                this.settings.publicData.publicApiKey &&
+                this.settings.privateData.privateApiKey &&
+                this.settings.publicData.publicStagingApiKey &&
+                this.settings.privateData.privateStagingApiKey &&
+                this.settings.publicData.publicTestApiKey &&
+                this.settings.privateData.privateTestApiKey
+            );
         },
     },
 };
--- src/components/Configuration/SettingsSummary.vue
+++ src/components/Configuration/SettingsSummary.vue
@@ -1,28 +1,67 @@
 <template>
-    <wwEditorFormRow label="Public API key">
+    <!-- PROD -->
+    <div class="my-3 label-sm flex items-center">Production</div>
+    <wwEditorFormRow label="Public prod API key">
         <div class="flex items-center body-2">
             <wwEditorIcon name="key" class="mr-2" />
             <div class="text-ellipsis">{{ settings.publicData.publicApiKey || 'not defined' }}</div>
         </div>
     </wwEditorFormRow>
-    <wwEditorFormRow label="Private API key">
+    <wwEditorFormRow label="Private prod API key">
         <div class="flex items-center body-2">
             <wwEditorIcon name="key" class="mr-2" />
             <div class="text-ellipsis">{{ privateApiKey || 'not defined' }}</div>
         </div>
     </wwEditorFormRow>
-    <wwEditorFormRow label="Public test API key" v-if="settings.publicData.publicTestApiKey">
+    <!-- STAGING -->
+    <div class="my-3 label-sm flex items-center">Staging</div>
+    <wwEditorFormRow label="Public staging API key" v-if="isStagingConfigValid">
+        <div class="flex items-center body-2">
+            <wwEditorIcon name="key" class="mr-2" />
+            <div class="text-ellipsis">{{ settings.publicData.publicStagingApiKey }}</div>
+        </div>
+    </wwEditorFormRow>
+    <wwEditorFormRow label="Private staging API key" v-if="isStagingConfigValid">
+        <div class="flex items-center body-2">
+            <wwEditorIcon name="key" class="mr-2" />
+            <div class="text-ellipsis">{{ privateStagingApiKey }}</div>
+        </div>
+    </wwEditorFormRow>
+    <div class="mb-3 body-sm content-warning flex items-center" v-if="isStagingConfigNotSet">
+        Not set => Production keys used in staging.
+    </div>
+    <div class="mb-3 label-sm content-alert flex items-center" v-if="isStagingConfigBroken">
+        <wwEditorIcon class="mr-1" name="warning" small />
+        {{
+            privateStagingApiKey
+                ? 'Private key is set but not public => Possible issues in staging.'
+                : 'Public key is set but not private => Possible issues in staging.'
+        }}
+    </div>
+    <!-- EDITOR -->
+    <div class="my-3 label-sm flex items-center">Editor</div>
+    <wwEditorFormRow label="Public editor API key" v-if="isEditorConfigValid">
         <div class="flex items-center body-2">
             <wwEditorIcon name="key" class="mr-2" />
             <div class="text-ellipsis">{{ settings.publicData.publicTestApiKey }}</div>
         </div>
     </wwEditorFormRow>
-    <wwEditorFormRow label="Private test API key" v-if="privateTestApiKey">
+    <wwEditorFormRow label="Private editor API key" v-if="isEditorConfigValid">
         <div class="flex items-center body-2">
-            <wwEditorIcon name="key" class="mr-2" />
             <div class="text-ellipsis">{{ privateTestApiKey }}</div>
         </div>
     </wwEditorFormRow>
+    <div class="mb-3 body-sm content-warning flex items-center" v-if="isEditorConfigNotSet">
+        Not set => Production keys used in the editor.
+    </div>
+    <div class="mb-3 label-sm content-alert flex items-center" v-if="isEditorConfigBroken">
+        <wwEditorIcon class="mr-1" name="warning" small />
+        {{
+            privateTestApiKey
+                ? 'Private key is set but not public => Possible issues in editor.'
+                : 'Public key is set but not private => Possible issues in editor.'
+        }}
+    </div>
 </template>
 
 <script>
@@ -31,11 +70,50 @@ export default {
         settings: { type: Object, required: true },
     },
     computed: {
+        // used in prod
         privateApiKey() {
             return (this.settings.privateData.privateApiKey || '').replace(/./g, '*');
         },
+        // used in staging
+        privateStagingApiKey() {
+            return (this.settings.privateData.privateStagingApiKey || '').replace(/./g, '*');
+        },
+        // used in editor
         privateTestApiKey() {
             return (this.settings.privateData.privateTestApiKey || '').replace(/./g, '*');
+        },
+
+        isProdConfigValid() {
+            return this.settings.publicData.publicApiKey && this.settings.privateData.privateApiKey;
+        },
+        isStagingConfigValid() {
+            return this.settings.publicData.publicStagingApiKey && this.settings.privateData.privateStagingApiKey;
+        },
+        isEditorConfigValid() {
+            return this.settings.publicData.publicTestApiKey && this.settings.privateData.privateTestApiKey;
+        },
+
+        isProdConfigNotSet() {
+            return !this.settings.publicData.publicApiKey && !this.settings.privateData.privateApiKey;
+        },
+        isStagingConfigNotSet() {
+            return !this.settings.publicData.publicStagingApiKey && !this.settings.privateData.privateStagingApiKey;
+        },
+        isEditorConfigNotSet() {
+            return !this.settings.publicData.publicTestApiKey && !this.settings.privateData.privateTestApiKey;
+        },
+
+        isStagingConfigBroken() {
+            return (
+                (this.settings.publicData.publicStagingApiKey && !this.settings.privateData.privateStagingApiKey) ||
+                (!this.settings.publicData.publicStagingApiKey && this.settings.privateData.privateStagingApiKey)
+            );
+        },
+        isEditorConfigBroken() {
+            return (
+                (this.settings.publicData.publicTestApiKey && !this.settings.privateData.privateTestApiKey) ||
+                (!this.settings.publicData.publicTestApiKey && this.settings.privateData.privateTestApiKey)
+            );
         },
     },
 };
